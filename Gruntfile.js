359e7645a399897cfc60b1c55af8edc4f7137dcc
James Cryer <info@jamescryer.com>
2013-02-24 13:25:29 +0000

    Fixing indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,38 +10,38 @@
 
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
 
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'tasks/*.js'
-			],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		},
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
 
-		phpunit: {
-			classes: {
-				dir: './php/tests/'
-			},
-			options: {
-				bin: 'vendor/bin/phpunit',
-				bootstrap: 'bootstrap.php',
+        phpunit: {
+            classes: {
+                dir: './php/tests/'
+            },
+            options: {
+                bin: 'vendor/bin/phpunit',
+                bootstrap: 'bootstrap.php',
                 configuration: 'phpunit.xml',
-				colors: true
-			}
-		}
-	});
+                colors: true
+            }
+        }
+    });
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	// By default, lint and run all tests.
-	grunt.registerTask('default', ['jshint', 'phpunit']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'phpunit']);
 
 };
